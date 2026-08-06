7fe4b9eddb40972ecf115a8763158a05833ac57c
Jake Savage <jakesavage31@gmail.com>
2026-08-06 13:27:53 +0000

    File changes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -24,9 +24,7 @@ export default function Logo({ variant = "horizontal", className }) {
       src={LOGO_URL}
       alt="ReportAL 360"
       className={className}
-      fittingType="fill"
-      focalPointX={0.5}
-      focalPointY={0.5}
+      fittingType="fit"
       originWidth={1024}
       originHeight={1024}
     />
